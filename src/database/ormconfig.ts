de6c5984dd993f5980e864c752fa58ec8a728bfc
Gabriel Azevedo <gmarquesazti@gmail.com>
2022-04-19 10:12:27 -0300

    feat: production environment

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -4,7 +4,7 @@ import { ConnectionOptions } from 'typeorm';
 
 dotenv.config();
 
-export default {
+const developmentEnv: ConnectionOptions = {
   type: 'postgres',
   host: 'capstone-postgres',
   port: 5432,
@@ -19,4 +19,20 @@ export default {
     entitiesDir: path.join(__dirname, '../entities'),
     migrationsDir: path.join(__dirname, '../migrations'),
   },
-} as ConnectionOptions;
+};
+
+const productionEnv: ConnectionOptions = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [path.join(__dirname, '../entities/**/*.*')],
+  migrations: [path.join(__dirname, '../migrations/**/*.*')],
+  cli: {
+    entitiesDir: path.join(__dirname, '../entities'),
+    migrationsDir: path.join(__dirname, '../migrations'),
+  },
+  ssl: { rejectUnauthorized: false },
+};
+
+export default process.env.NODE_ENV === 'production'
+  ? productionEnv
+  : developmentEnv;
